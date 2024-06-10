c69cd6e5aa0021235df932cc908543296a6695c1
mtai0524 <115647147+mtai0524@users.noreply.github.com>
2024-06-10 10:14:25 +0700

    fix: navbar ui mobile

--- BlazorSound/obj/Release/net6.0/PubTmp/Out/wwwroot/js/musicPlayerInterop.js
+++ BlazorSound/obj/Release/net6.0/PubTmp/Out/wwwroot/js/musicPlayerInterop.js
@@ -1,10 +1,56 @@
-﻿// musicPlayerInterop.js
-window.musicPlayerInterop = {
-    playAudio: function (audioSrc) {
-        var audioElement = document.querySelector("audio");
-        if (audioElement) {
-            audioElement.src = audioSrc;
-            audioElement.play();
+﻿window.musicPlayerInterop = {
+    playTimeIntervalId: null,
+
+    getCookie: function (name) {
+        const value = '; ' + document.cookie;
+        const parts = value.split('; ' + name + '=');
+        if (parts.length === 2) return parts.pop().split(';').shift();
+    },
+
+    setCookie: function (name, value, days) {
+        var expires = "";
+        if (days) {
+            var date = new Date();
+            date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+            expires = "; expires=" + date.toUTCString();
+        }
+        document.cookie = name + "=" + (value || "") + expires + "; path=/";
+    },
+
+    deletePlayTimeCookies: function () {
+        const cookies = document.cookie.split("; ");
+        cookies.forEach(cookie => {
+            if (cookie.startsWith("playTime_")) {
+                const cookieName = cookie.split("=")[0];
+                document.cookie = cookieName + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+            }
+        });
+    },
+
+    updatePlayTime: function (selectedFile) {
+        var audio = document.getElementById('audioPlayerContainer');
+
+        if (this.playTimeIntervalId) {
+            clearInterval(this.playTimeIntervalId);
+        }
+
+        if (audio != null) {
+            this.playTimeIntervalId = setInterval(function () {
+                var currentTime = audio.currentTime;
+                var cookieName = "playTime_" + selectedFile;
+                musicPlayerInterop.setCookie(cookieName, currentTime, 7);
+            }, 500);
+        }
+    },
+
+    playMusicFromSavedTime: function (selectedFile) {
+        var savedTime = musicPlayerInterop.getCookie("playTime_" + selectedFile);
+        if (savedTime !== undefined && savedTime !== null) {
+            var audio = document.getElementById('audioPlayerContainer');
+            if (audio !== null) {
+                audio.currentTime = parseFloat(savedTime);
+                audio.play();
+            }
         }
     }
 };
