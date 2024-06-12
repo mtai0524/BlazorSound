e2c7ed240c67f7ccd583c69f24af437c125a5532
mtai0524 <115647147+mtai0524@users.noreply.github.com>
2024-06-12 19:39:05 +0700

    style: ui sportify home

--- BlazorSound/obj/Release/net6.0/BlazorSound.AssemblyInfo.cs
+++ BlazorSound/obj/Release/net6.0/BlazorSound.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorSound")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d73be58c202616832358834e1bcc24d7a934cd2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b45eded213eb2d575ef159842574b79855f41393")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorSound")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorSound")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
